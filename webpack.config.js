416a69ea8e2f4fb987ccb391f0f5b7d54b8f0b77
fisherman0825 <fisherman0825dd@gmail.com>
2019-04-07 00:04:07 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                use: [{
+                    loader: 'awesome-typescript-loader'
+                }]
+            },
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'source-map-loader',
+                    options: {
+                        enforce: 'pre',
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }]
+            }
+        ]
+    },
+    resolve: {
+        modules: [
+            "node_modules",
+            path.resolve('./src'),
+        ],
+        extensions: ['.tsx', '.js', '.ts', 'json']
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    devtool: "source-map",
+    devServer: {
+      contentBase: './dist'
+    }
+  };
